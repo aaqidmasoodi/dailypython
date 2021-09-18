3cd69f76d638a2a9c51e4d5380c14f0a21a7b37b
aaqidmasoodi <aaqidmasoodi@icloud.com>
2021-09-18 22:48:51 +0530

    sets

---
+++ DailyPython/DataTypes/Collections/sets/set2.py
@@ -0,0 +1,23 @@
+# name = ''
+name = str()
+print(type(name))
+
+# my_list = []
+my_list = list()
+print(type(my_list))
+
+# my_tup = ()
+my_tup = tuple()
+print(type(my_tup))
+
+# my_set = {} # type set?? -->> this is a dictionary
+
+# no room for cheating
+my_set = set() # 
+print(type(my_set))
+
+
+# mutable
+# immuateble
+# app -->> data transfer
+# api -->> access point (application programming interface)
---
+++ DailyPython/DataTypes/Collections/sets/sets.py
@@ -0,0 +1,53 @@
+# sets
+# why sets
+# problem specific
+
+engg_courses = {'Physics', 'Math', 'History', 'CS','History', 'History'} # 10 ** 12
+
+art_courses = {'Physics', 'Math', 'Desgin', 'CS'}
+
+# if user_color in colors - allow the user else decline
+
+# colors = {'Red', 'Yellow', 'Black'} # mutable
+
+
+
+
+# for item in engg_courses:
+#     print(item)
+
+
+# colors.add('Purple')
+
+# for item in engg_courses:
+#     print(item)
+
+# art_courses = {'Physics', 'Art', 'management', 'CS'}
+
+# lists, string, tuples - preservers
+
+# print(type(engg_courses))
+print(engg_courses)
+print(art_courses)
+
+# print('Physics' in engg_courses) # much faster in sets than in lists
+
+# print(engg_courses.difference(art_courses)) # hard problem
+
+print(art_courses.intersection(engg_courses))
+
+# changing the set
+# add()
+# remove()
+
+# set operation - LOOK UP THESE IN PARTICULAR
+# w3schools.com// check set
+# intersection()
+# union()
+# difference()
+
+
+# symmetric difference()
+
+
+
