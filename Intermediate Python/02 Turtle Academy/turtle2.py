5ce99e6b3e020a806c858839ff7ceb28474b9563
aaqidmasoodi <aaqidmasoodi@icloud.com>
2021-10-29 20:17:50 +0530

    moving the ball and collsions

--- Intermediate Python/02 Turtle Academy/turtle2.py
+++ Intermediate Python/02 Turtle Academy/turtle2.py
@@ -15,17 +15,34 @@ screen.tracer(0) # killing the animation
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('circle') # so that it looks like a ball
+ball.penup()
 
 
+dx = 5
+dy = 5
+
+
+# Main Loop
 while True:
     
-    print('Hello')
-    ball.setx(ball.xcor() + 1) # do you understand what this is doing?
-   # travelling speed is not fixed?
-    
+    # set the x # get the x
+    ball.setx(ball.xcor() + dx) # varibles
+    ball.sety(ball.ycor() + dy) 
+
+    # print((int(ball.xcor()), int(ball.ycor())))
+
+    if ball.ycor() == 290 or ball.ycor() == -290:
+        dy = -dy
+ 
+
+    if ball.xcor() == -390 or ball.xcor() == 390:
+        dx = -dx
+
+
+
+
     screen.update()
-    time.sleep(1/60) # your thread will sleep for 1 second
-    # limit the iteration rate
-    # less burden on system
+    time.sleep(1/60) 
+ 
 
     
---
+++ Intermediate Python/02 Turtle Academy/turtle_cheatsheet.py
@@ -0,0 +1,15 @@
+# introduced turtle
+# how to create a screen
+    # changing various screen attributes
+
+# how to create turlte object
+    # forward()
+    # left()
+    # right()
+
+    # setx() & sety()
+    # xcor() & ycor()
+
+
+    # penup() # lifts the pen up
+    # pendown() # puts the pen down
