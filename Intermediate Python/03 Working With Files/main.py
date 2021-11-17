3e1ae30ea031e9c67043195293d42282afd3e686
aaqidmasoodi <aaqidmasoodi@icloud.com>
2021-11-17 20:11:36 +0530

    EOF

---
+++ Intermediate Python/03 Working With Files/main.py
@@ -0,0 +1,40 @@
+# . -->> BASE_DIR -->> DAILY PYTHON FOLDER
+
+BASE_DIR = '/home/neo/Documents/Workshop/dailypython'
+
+with open('newfile_zt.txt', 'r') as f:
+    pass
+
+
+# rb
+# wb
+
+
+# file path
+# os
+# sys
+# itertools
+# regex
+# random
+# csv
+# json
+# 
+# .
+# .
+# .
+
+# THEY HAVE CERTICATE
+
+# External
+# BeautifulSoup
+# TwitterAPI
+
+
+# modules -->> buildin modules
+# pip -- Package manager for python 
+# PyPi -->> 
+
+
+# NOW YOU CAN CHECK YOUTUBE ALSO
+# TRY TO DO AS MANY AS YOU CAN
+# POINT IS TO LEARN
