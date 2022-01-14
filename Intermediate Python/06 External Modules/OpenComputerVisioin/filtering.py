3c1e6335f1d834932e9d81d2dca71fc7b1abdf9b
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-01-14 20:45:59 +0530

    contours

--- Intermediate Python/06 External Modules/OpenComputerVisioin/filtering.py
+++ Intermediate Python/06 External Modules/OpenComputerVisioin/filtering.py
@@ -5,22 +5,30 @@ import numpy as np
 
 window = tk.Tk()
 
+label_low = tk.Label(window, text='LOWER BOUND',pady=30)
+
 # creating three sliders for HUE, SATURATION, VALUE for LowerB
 low_H = tk.Scale(window, from_=0, to=359, orient='horizontal')
 low_S = tk.Scale(window, from_=0, to=359, orient='horizontal')
 low_V = tk.Scale(window, from_=0, to=359, orient='horizontal')
 
+label_low.pack()
 low_H.pack()
 low_S.pack()
 low_V.pack()
 
-# low_h.set(int)
+
+# i've also added some labels and stuff for your understanding...
+# low_h.set(int) # to set an initial value for a slider
+
+label_high = tk.Label(window, text='HIGHER BOUND', pady=30)
 
 # creating three sliders for HUE, SATURATION, VALUE for UpperB
 High_H = tk.Scale(window, from_=0, to=359, orient='horizontal')
 High_S = tk.Scale(window, from_=0, to=359, orient='horizontal')
 High_V = tk.Scale(window, from_=0, to=359, orient='horizontal')
 
+label_high.pack()
 High_H.pack()
 High_S.pack()
 High_V.pack()
@@ -42,6 +50,8 @@ while True:
 
  # lowerB
  low_b = np.array([low_H.get(), low_S.get(), low_V.get()])
+
+ # HigherB
  high_b = np.array([High_H.get(), High_S.get(), High_V.get()])
 
  # creating the mask
